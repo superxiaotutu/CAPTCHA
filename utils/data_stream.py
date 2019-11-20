49b10837923b08434470cf597fb449231d1a9dad
KirinNg <kirinng0709@gmail.com>
2019-11-20 14:50:40 +0800

    first

---
+++ utils/data_stream.py
@@ -0,0 +1,26 @@
+
+class CAPTCHA_creater:
+    def __init__(self, height, length, difficult):
+        pass
+
+    def get_batch(self, batch_size):
+        # return imgs, labels
+        pass
+
+    def get_one(self):
+        # return img, label
+        pass
+
+    # 这俩推荐使用pickle
+    def save_batch(self, path):
+        pass
+
+    def load_batch(self, path):
+        pass
+
+    # 这俩存图片就行
+    def save_img(self, path):
+        pass
+
+    def load_img(self, path):
+        pass
