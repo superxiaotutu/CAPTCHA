e008447a5836a65ad85f0118f76e7a41faa38811
KirinNg <kirinng0709@gmail.com>
2019-11-21 10:08:40 +0800

    config&train_method

--- main.py
+++ main.py
@@ -1,25 +1,24 @@
 import tensorflow as tf
 from utils.data_stream import *
-
-# config
-diff = "easy"
-image_height, image_width, image_channel = 10, 10, 3
-NumCAPTCHA, NumAlb = 4, 26
+from utils.config import *
+from utils.train_method import *
 
 # Ph
 input_img = tf.placeholder(tf.float32, [None, image_height, image_width, image_channel])
 input_label = tf.placeholder(tf.float32, [None, NumCAPTCHA, NumAlb])
+keep_prob = tf.placeholder(tf.float32)
 
 # DATA
 data_stream = CAPTCHA_creater(image_height, image_width, diff)
 
 # process
 
-# train model
-def CNN_model():
-    pass
 
+# train model
+CNN_end_points = CNN_model(input_img, input_label, keep_prob)
 
 # train op
 
+
 # main loop
+
---
+++ utils/config.py
@@ -0,0 +1,4 @@
+# config
+diff = "easy"
+image_height, image_width, image_channel = 10, 10, 3
+NumCAPTCHA, NumAlb = 4, 26
