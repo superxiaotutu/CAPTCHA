4f205530814572f74d633277d963581c709a7f4d
Lan <lanzhang1107@gmail.com>
2019-11-22 14:47:15 +0800

    old version

--- utils/process.py
+++ utils/process.py
@@ -9,24 +9,32 @@ from captcha.image import DEFAULT_FONTS, ImageCaptcha
 from skimage.util import random_noise
 import matplotlib.pyplot as plt
 
-image_channel = 3
-image_height = 64
-image_width = 192
+def head_B(self, input, phi=0.8):
+        kernel_3_1 = np.ones([1, 1, 3, 1]) / 3
+        kernel_1_3 = np.ones([1, 1, 1, 3])
+        a = tf.nn.conv2d(input, kernel_3_1, strides=[1, 1, 1, 1], padding="SAME")
+        b = tf.nn.conv2d(a, kernel_1_3, strides=[1, 1, 1, 1], padding="SAME")
+        S = 1 / (1 + tf.exp(- 20 * (b - phi)))
+        return S
 
+    def head_Guss(self, inputs, kerStd=0.8):
+        def getGuessValue(kerStd, posX, posY):
+            return 1. / (2. * np.pi * (np.power(kerStd, 2))) * np.exp(
+                -(np.power(posX, 2) + np.power(posY, 2)) / (2. * (np.power(kerStd, 2))))
 
-def binary(image):
-    image = image.convert('L')
-    image = image.point(lambda x: 255 if x > np.mean(image) else 0)
-    image = image.convert('RGB')
-    return image
+        def getGuessKernel(kerStd):
+            K11 = np.eye(3) * getGuessValue(kerStd, -1, 1)
+            K12 = np.eye(3) * getGuessValue(kerStd, 0, 1)
+            K13 = np.eye(3) * getGuessValue(kerStd, 1, 1)
+            K21 = np.eye(3) * getGuessValue(kerStd, -1, 0)
+            K22 = np.eye(3) * getGuessValue(kerStd, 0, 0)
+            K23 = np.eye(3) * getGuessValue(kerStd, 1, 0)
+            K31 = np.eye(3) * getGuessValue(kerStd, -1, -1)
+            K32 = np.eye(3) * getGuessValue(kerStd, 0, -1)
+            K33 = np.eye(3) * getGuessValue(kerStd, 1, -1)
+            kernel = tf.constant(np.array([[K11, K12, K13], [K21, K22, K23], [K31, K32, K33]]),
+                                 dtype=tf.float32)  # 3*3*4*4
+            return kernel
 
-
-def add_gauss(image, radius=2):
-    image = cv2.GaussianBlur(image, (radius, radius))
-    return image
-
-
-def sigmoid(image, w, t):
-    img = np.array(image)
-    img = (img[:, :, 0] * 299 + img[:, :, 1] * 587 + img[:, :, 2] * 114 + 500) / 1000
-    return img
+        kernel = getGuessKernel(kerStd)
+        return tf.nn.conv2d(inputs, kernel, strides=[1, 1, 1, 1], padding="SAME")
